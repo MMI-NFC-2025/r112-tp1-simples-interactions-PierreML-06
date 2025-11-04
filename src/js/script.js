ac6ffe8763961b7ded24a3ef0a723c2d6be30ecd
PierreML-06 <pierre.mouilleseaux-lhuillier@edu.univ-fcomte.fr>
2025-11-04 14:56:49 +0100

    script fini

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,21 @@
 // TODO
+// 1)
+  const aMettreEnRouge = document.querySelector('#a-mettre-en-rouge');
+    aMettreEnRouge.style.color = 'red';
+
+
+// 2)
+  const enRougeSuiteAClick = document.querySelector('#en-rouge-suite-a-click');
+  if (enRougeSuiteAClick) {
+    enRougeSuiteAClick.addEventListener('click', () => {
+      enRougeSuiteAClick.style.color = 'red';
+    });
+  }
+
+// 3)
+  const collH2 = document.querySelectorAll('h2');
+  collH2.forEach((elm) => {
+    elm.addEventListener('click', (evt) => {
+      evt.target.style.color = 'red';
+    });
+  });
